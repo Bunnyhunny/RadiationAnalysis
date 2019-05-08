cd21dc2270172753f5a0dc6f4dec1a39b4add28b
Xuezhen Liang <suzan.leung@yahoo.com>
2019-05-07 20:05:27 -0700

    Create reverseGeo.py

---
+++ ConvertGeoCodeToGeoLocation/reverseGeo.py
@@ -0,0 +1,69 @@
+
+import pandas as pd, requests, logging, time
+
+lfh = logging.FileHandler('reverse_geocoder.log', mode='w', encoding='utf-8')
+lfh.setFormatter(logging.Formatter('%(levelname)s %(asctime)s %(message)s'))
+log = logging.getLogger('reverse_geocoder')
+log.setLevel(logging.INFO)
+log.addHandler(lfh)
+log.info('process started')
+
+df = pd.read_csv('radiation_usa.csv', encoding='utf-8', names=["time", "lat", "long", "value"])
+df['geocode_data'] = ''
+df['county'] = ''
+df['state'] = ''
+df['country'] = ''
+
+print(df[:5])
+
+
+api_key = "xxxxxxx" #use your own API
+
+
+def reverse_geocode(latlng):
+    time.sleep(0.1)
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={}' + "&key={}".format(api_key)
+    request = url.format(latlng)
+    log.info(request)
+    response = requests.get(request)
+    data = response.json()
+    if 'results' in data and len(data['results']) > 0:
+        return data['results'][0]
+
+df['latlng'] = df.apply(lambda row: '{},{}'.format(row['lat'], row['long']), axis=1)
+df['geocode_data'] = df['latlng'].map(reverse_geocode)
+
+print(df[:5])
+
+def parse_county(geocode_data):
+    if (not geocode_data is None) and ('address_components' in geocode_data):
+        for component in geocode_data['address_components']:
+            if 'administrative_area_level_2' in component['types']:
+                return component['long_name']
+    return None
+
+
+def parse_state(geocode_data):
+    if (not geocode_data is None) and ('address_components' in geocode_data):
+        for component in geocode_data['address_components']:
+            if 'administrative_area_level_1' in component['types']:
+                return component['long_name']
+    return None
+
+
+def parse_country(geocode_data):
+    if (not geocode_data is None) and ('address_components' in geocode_data):
+        for component in geocode_data['address_components']:
+            if 'country' in component['types']:
+                return component['long_name']
+    return None
+
+
+df['county'] = df['geocode_data'].map(parse_county)
+df['state'] = df['geocode_data'].map(parse_state)
+df['country'] = df['geocode_data'].map(parse_country)
+
+print(len(df))
+print(df[:5])
+
+df.to_csv('radiation_city_usa.csv', encoding='utf-8', index=False)
